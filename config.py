2b623de229b2605dc374a2936697d7fc6e009743
complicat9d <ivanov.stuff@mail.ru>
2024-11-29 12:10:16 +0300

    update docker-compose.yml and config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
-    MONGO_HOST: str = "localhost"
+    MONGO_HOST: str = "db"
     MONGO_PORT: int = 27017
     MONGO_DB_NAME: str = "main"
 
